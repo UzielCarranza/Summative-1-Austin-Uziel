01d885513f603f7d71e7ca5d79a49c12095f3797
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-20 16:45:03 -0400

    component that contains all routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import {Tshirts} from "./tshirt/Tshirts";
+import {Games} from "./games/Games";
+import {GameConsoles} from "./gameConsoles/GameConsoles";
+import {DataSource} from "./utils/DataSource";
+import {getServerData} from "./utils/getServerData";
+import {Home} from "./Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn data
-        </a>
-      </header>
-    </div>
-  );
+    return (<>
+            <Routes>
+                <Route path="/" element={
+                    <Home/>
+                }/>
+                <Route path="/tshirts" element={
+                    <DataSource getDataFunc={getServerData('http://localhost:8080/tshirt')}
+                                resourceName="tshirts">
+                        <Tshirts/>
+                    </DataSource>}/>
+                }/>
+                <Route path="/games" element={
+                    <DataSource getDataFunc={getServerData('http://localhost:8080/game')}
+                                resourceName="games">
+                        <Games/>
+                    </DataSource>}/>
+                }/>
+                <Route path="/consoles" element={
+                    <DataSource getDataFunc={getServerData('http://localhost:8080/gameConsole')}
+                                resourceName="consoles">
+                        <GameConsoles/>
+                    </DataSource>}/>
+                <Route
+                    path="*"
+                    element={
+                        <main style={{padding: "1rem"}}>
+                            <p>There's nothing here! 404</p>
+                        </main>
+                    }
+                />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
