11d0fd7a4bf7daef3d183b3ae5b30716ce329a6d
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-21 12:46:04 -0400

    completed get by size search funtionality

--- src/tshirt/Tshirts.js
+++ src/tshirt/Tshirts.js
@@ -2,17 +2,37 @@ import {useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 import Modal from "../utils/Modal";
 import ModalPostRequest from "../utils/ModalPostRequest";
+import axios from "axios";
 
 export const Tshirts = (data) => {
     const navigate = useNavigate()
     const [tShirtsData, setTShirtsData] = useState(null);
+    const [tShirtsDataBackUp, setTShirtsDataBackUp] = useState(null);
+    const [searchBySize, setSearchBySize] = useState(null);
     // const [deleteTShirt, setDeleteTShirt] = useState(null);
 
     useEffect(() => {
         if (data.tshirts !== null) {
             setTShirtsData(data);
+            setTShirtsDataBackUp(data)
         }
     }, [data])
+
+    const searchByCategory = () => {
+        const response = axios.get(`http://localhost:8080/tshirt/size/${searchBySize}`)
+            .then(response => {
+                if (response.data.length === 0) {
+                    alert("No results found")
+                }
+                const searchByTShirtSizeResults = {tshirts: response.data};
+                setTShirtsData(searchByTShirtSizeResults)
+            })
+    }
+    const resetSearch = () => {
+        document.getElementById("search-by-size").value = null;
+        setSearchBySize(null)
+        setTShirtsData(tShirtsDataBackUp)
+    }
 
     // const deleteById = () => {
     //
@@ -23,9 +43,12 @@ export const Tshirts = (data) => {
     return tShirtsData ? (
         <div className="body-container">
             <div className="container scroll">
+                <input id="search-by-size" type="text" onChange={(e) => setSearchBySize(e.target.value)}
+                       placeholder="search by size"/>
+                <button onClick={searchByCategory}>search</button>
+                <button onClick={resetSearch}>reset</button>
                 {tShirtsData.tshirts.map((item, i) => (
                     <div key={tShirtsData.tshirts[i].tshirtId} className="product-wrapper">
-
                         <img
                             className="product-img"
                             src="//upload.wikimedia.org/wikipedia/commons/thumb/1/13/Replace_this_image_%28building%29.svg/100px-Replace_this_image_%28building%29.svg.png"
@@ -55,7 +78,8 @@ export const Tshirts = (data) => {
             </div>
 
             <div className="wrapper-buttons">
-                <ModalPostRequest showTShirtPostRequestForm={true} showGamesPostRequestForm={false} showGameConsolesPostRequestForm={false}/>
+                <ModalPostRequest showTShirtPostRequestForm={true} showGamesPostRequestForm={false}
+                                  showGameConsolesPostRequestForm={false}/>
                 <button
                     onClick={() => navigate("/games")}
 
