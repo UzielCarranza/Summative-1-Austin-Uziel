9c2e13ce88f4b836d2fb58fdf72b52161be26422
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-21 00:21:38 -0400

    added modal that will handle posts requests

---
+++ src/utils/ModalPostRequest.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import {useEffect, useState} from "react";
+import {TShirtPostRequest} from "../tshirt/TShirtPostRequest";
+
+const ModalBackground = styled.div`
+position: fixed;
+z-index: 4;
+left: 0;
+top: 0;
+width: 100%;
+height: 100%;
+overflow: auto;
+background-color: rgb(0,0,0,0.5);
+`;
+
+const ModalBody = styled.div`
+background-color: rgba(220,220,220);
+margin: 10% auto;
+padding: 20px;
+width: 90%;
+height: auto;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+`;
+
+
+function ModalPostRequest(showTShirtPostRequest) {
+    const [shouldShow, setShouldShow] = useState(false);
+
+    return (
+        <>
+            <div className="flex justify-center">
+
+                <button onClick={() => setShouldShow(true)}>
+                    create
+                </button>
+            </div>
+            {shouldShow && (
+                <ModalBackground>
+                    <ModalBody onClick={(e) => e.stopPropagation()}>
+
+                        {showTShirtPostRequest.showTShirtPostRequest === true ?  <TShirtPostRequest/> : <p>none</p>}
+
+                        <div>
+                            <button className="close-modal-btn"
+                                    onClick={() => setShouldShow(false)}>
+                                Close
+                            </button>
+                            <button className="close-modal-btn">
+                                Submit
+                            </button>
+                        </div>
+                    </ModalBody>
+                </ModalBackground>
+            )}
+        </>
+    );
+}
+
+
+export default ModalPostRequest;
