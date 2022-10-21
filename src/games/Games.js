6d06a4d479d16cd87ba405befbf33080f461ef52
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-20 21:40:37 -0400

    Added functionality to edit games

--- src/games/Games.js
+++ src/games/Games.js
@@ -1,5 +1,7 @@
 import {useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {AiFillEdit} from "react-icons/ai";
+import Modal from "../utils/Modal";
 
 export const Games = (data) => {
     const navigate = useNavigate()
@@ -9,7 +11,7 @@ export const Games = (data) => {
             setGamesData(data);
         }
     }, [data])
-    console.log(gamesData)
+    console.log(data)
     return gamesData ? (
 
         <div className="body-container">
@@ -20,14 +22,24 @@ export const Games = (data) => {
                             className="product-img"
                             src="//upload.wikimedia.org/wikipedia/commons/thumb/1/13/Replace_this_image_%28building%29.svg/100px-Replace_this_image_%28building%29.svg.png"
                             alt="placeholder"/>
-
                         <div className="product-properties">
                             <h1>Price: {gamesData.games[i].price}</h1>
                             <h1>Quantity: {gamesData.games[i].quantity}</h1>
-                            <h1>Size: {gamesData.games[i].size}</h1>
-                            <h1>Color: {gamesData.games[i].color}</h1>
+                            <h1>Esrb Rating: {gamesData.games[i].esrbRating}</h1>
                             <h1>Description: {gamesData.games[i].description}</h1>
+                            <h1> Studio: {gamesData.games[i].studio}</h1>
+                            <h1> Title: {gamesData.games[i].title}</h1>
                         </div>
+                        <Modal obj={null} game={{
+                            id: gamesData.games[i].gameId,
+                            price: gamesData.games[i].price,
+                            quantity: gamesData.games[i].quantity,
+                            esrbRating: gamesData.games[i].esrbRating,
+                            description: gamesData.games[i].description,
+                            studio: gamesData.games[i].studio,
+                            title: gamesData.games[i].title
+                        }}>
+                        </Modal>
                     </div>
                 ))}
             </div>
